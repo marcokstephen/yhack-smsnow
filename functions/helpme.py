39b98d662b33f238d72a1e9215779624693b382b
marcokstephen <marcok.stephen@gmail.com>
2014-11-02 02:42:37 -0500

    Updating help file

--- functions/helpme.py
+++ functions/helpme.py
@@ -2,11 +2,12 @@ class GetHelp():
    @staticmethod
    def gethelp():
       return """Possible commands: (replace brackets)\n
-stock price (GOOG)
-weather (san francisco)
-weather forecast (toronto)
-set alarm (8am tomorrow)
-remind me (pay the phone bill)
-translate "(phrase)" (korean)
-directions from (yale university) to (harvard university)
-attractions near (Kingston Rd & Westney Rd)"""
+* stock price (GOOG)
+* weather (san francisco)
+* weather forecast (toronto)
+* set alarm (8am tomorrow)
+* remind me (pay the phone bill)
+* translate "(phrase)" (korean)
+* directions from (yale university) to (harvard university)
+* attractions near (Kingston Rd & Westney Rd)
+* find hotel near (Yale University)"""
